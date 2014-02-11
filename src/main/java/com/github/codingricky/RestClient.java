4a8028595a467a6a2302dd2007f4e43838b08b8e
Ricky <ricky@dius.com.au>
2014-02-12 07:38:46 +1100

    updated to use program args

--- src/main/java/com/github/codingricky/RestClient.java
+++ src/main/java/com/github/codingricky/RestClient.java
@@ -4,24 +4,20 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import org.apache.commons.codec.digest.DigestUtils;
-import us.monoid.web.JSONResource;
 import us.monoid.web.Resty;
 
 public class RestClient {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        try {
-            String publicKey = System.getenv("PUBLIC_KEY");
-            String privateKey = System.getenv("PRIVATE_KEY");
-            String ts = "1";
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        String publicKey = args[0];
+        String privateKey = args[1];
+        long timeStamp = System.currentTimeMillis();
+        int limit = 5;
 
-            String stringToHash = ts + privateKey + publicKey;
-            String hash = DigestUtils.md5Hex(stringToHash);
+        String stringToHash = timeStamp + privateKey + publicKey;
+        String hash = DigestUtils.md5Hex(stringToHash);
 
-            String url = "http://gateway.marvel.com/v1/public/characters?ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash;
-            String output = new Resty().text(url).toString();
-            System.out.println(output);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String url = String.format("http://gateway.marvel.com/v1/public/characters?timeStamp=%i&apikey=%s&hash=%s&limit=%i", timeStamp, publicKey, hash, limit);
+        String output = new Resty().text(url).toString();
+        System.out.println(output);
     }
 }
