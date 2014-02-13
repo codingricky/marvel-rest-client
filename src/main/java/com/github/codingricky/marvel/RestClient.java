c9af9b322dada64fdd38b98b9fdef3cc5e8b046b
Ricky <ricky@dius.com.au>
2014-02-13 22:43:14 +1100

    added test and travis.yml

--- src/main/java/com/github/codingricky/marvel/RestClient.java
+++ src/main/java/com/github/codingricky/marvel/RestClient.java
@@ -1,13 +1,12 @@
 package com.github.codingricky.marvel;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.codingricky.marvel.model.MarvelCharacter;
 import com.github.codingricky.marvel.model.Result;
 import us.monoid.web.Resty;
-
-import java.io.IOException;
-import java.util.List;
 
 public class RestClient {
 
@@ -37,14 +36,5 @@ public class RestClient {
         JavaType javaType = objectMapper.getTypeFactory().constructParametricType(Result.class, MarvelCharacter.class);
         return objectMapper.readValue(result, javaType);
 
-    }
-
-    public static void main(String[] args) throws IOException {
-        String publicKey = args[0];
-        String privateKey = args[1];
-        final RestClient restClient = new RestClient(privateKey, publicKey);
-
-        final Result<MarvelCharacter> characters = restClient.getCharacters(new ParameterBuilder().withLimit(5).create());
-        System.out.println(characters.toString());
     }
 }
---
+++ src/test/java/com.github.codingricky.marvel/RestClientTest.java
@@ -0,0 +1,40 @@
+package com.github.codingricky.marvel;
+
+import java.io.IOException;
+
+import com.github.codingricky.marvel.model.MarvelCharacter;
+import com.github.codingricky.marvel.model.Result;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class RestClientTest {
+
+    private RestClient restClient;
+
+    @Before
+    public void before() {
+        String publicKey = System.getenv("PUBLIC_KEY");
+        if (StringUtils.isBlank(publicKey)) {
+            publicKey = System.getProperty("PUBLIC_KEY");
+        }
+        String privateKey = System.getenv("PRIVATE_KEY");
+        if (StringUtils.isBlank(privateKey)) {
+            privateKey = System.getProperty("PRIVATE_KEY");
+        }
+
+        assertTrue("Public key is blank", !StringUtils.isBlank(publicKey));
+        assertTrue("Private key is blank", !StringUtils.isBlank(privateKey));
+
+        restClient = new RestClient(privateKey, publicKey);
+    }
+
+    @Test
+    public void testGetCharacters() throws IOException {
+        Result<MarvelCharacter> characters = restClient.getCharacters(new ParameterBuilder().withLimit(5).create());
+        assertThat(characters.getData().getCount()).isEqualTo(5);
+    }
+}
